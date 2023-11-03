8316e62c9aa1f10c677d00c9a06f7867e775de53
Siasor88 <76254093+Siasor88@users.noreply.github.com>
2023-11-03 18:49:49 +0330

    final version

--- Hw2.py
+++ Hw2.py
@@ -1,6 +1,7 @@
 # General verilog code generatoer
 import typing
-from typing import List 
+from typing import List
+
 
 class System:
     def __init__(self, states: List[int], transition):
@@ -10,48 +11,52 @@ class System:
 
     def all_states(self):
         return self.states
-    
+
     def state_events(self, state):
         return self.transition[state]
+
     def all_events(self):
         if self.all_events_available:
-            return self.events 
+            return self.events
         else:
             self.all_events_available = True
             self.events = set()
-            for state in self.transittion.keys():
-                self.events.add(self.transition[state])
+            for state in self.transition.keys():
+                for key in (self.transition[state].keys()):
+                    self.events.add(key)
+        self.events = sorted(list(self.events))
         return self.events
-class Genertator:
 
-    def get_state_binary_bit(self, state: int, n:int):
+
+class Generator:
+    def get_state_binary_bit(self, state: int, n: int):
         if n < state: raise Exception('Invalid Input')
         res = str(n) + '\'b'
         for i in range(1, n + 1):
-            if i == state:
-                res +='1'
-            else: res += '0'
+            if i == n - state + 1:
+                res += '1'
+            else:
+                res += '0'
         return res
 
-    
-    def generate_verilog_code(self, sys:System):
+    def generate_verilog_code(self, sys: System):
         code = 'module System(input start_system,\n'
         inputs = sys.all_events()
 
-        outputs = ['action_' + s for s in sys.all_states()]
+        outputs = ['action_' + str(s) for s in sys.all_states()]
         for input in inputs:
-            code += 'input ' + input + ',\n'
+            code += '\t\tinput ' + input + ',\n'
 
         for output in outputs:
-            code += 'output reg' + output + ', \n'
+            code += '\t\toutput reg ' + output + ', \n'
 
-        code += 'output reg system_failure)\n'
-        state_count = len(sys.all_states[sys])
-        code += 'reg ['+str(state_count)+':0] state;\n'
-        code += 'always@(start_system '
+        code += '\t\toutput reg system_failure\n);\n'
+        state_count = len(sys.all_states())
+        code += 'reg [' + str(state_count) + ':1] state;\n'
+        code += 'always@(\n\t\tstart_system '
         for event in sys.all_events():
-            code += ' or\n' + event
-        code += '\n)\n\tbegin\n'
+            code += ' or\n\t\t' + event
+        code += '\n)\nbegin\n'
 
         init_state = str(state_count) + '\'b' + '0' * state_count
 
@@ -60,24 +65,56 @@ class Genertator:
         code += '\tsystem_failure <= 1\'b0;\n'
         code += 'end\n'
 
-        for event in sys.all_events(): 
-            code += 'else if (' + event + ')\n begin\n'
-            code += '\tcase(state) begin\n'
+        for event in sys.all_events():
+            code += 'else if (' + event + ')\nbegin\n'
+            code += '\tcase(state)\n'
             for state in sys.all_states():
-                code += self.get_state_binary_bit(state, state_count) + ':\n'
-                code += '\t\tbegin\n' 
+                code += '\t' + self.get_state_binary_bit(state, state_count) + ':\n'
+                code += '\t\tbegin\n'
                 next_state = None
                 if event in sys.transition[state]:
-                    next_state = self.get_state_binary_bit(sys.transition[state][event])
+                    next_state = self.get_state_binary_bit(sys.transition[state][event], state_count)
                 else:
                     code += '\t\tsystem_failure <= 1\'b1;\n'
                     next_state = init_state
-                code += 'state <= ' + next_state + ';\n'
-            code += 'default: '
+                code += '\t\tstate <= ' + next_state + ';\n'
+                code += '\t\tend\n'
+            code += '\tdefault: \n'
+            code += '\t\tbegin\n'
+            code += '\t\tsystem_failure <= 1\'b1;\n'
+            code += '\t\tstate <= ' + init_state + ';\n'
+            code += '\t\tend\n'
+            code += '\tendcase\n'
+            code += 'end\n'
 
-
-            code += '\tend'
-            code += 'end'
+        code += 'always@(state) begin \n'
+        code += '\tcase(state) \n'
+        for state in sys.all_states():
+            code += '\t' + self.get_state_binary_bit(state, state_count) + ':\n'
+            code += '\t\tbegin\n'
+            code += '\t\t' + f'action_{state} <= 1\'b1;\n'
+            for st in sys.all_states():
+                if st != state:
+                    code += '\t\t' + f'action_{st} <= 1\'b0;\n'
+            code += '\t\tend\n'
+        code += '\tdefault: \n'
+        code += '\t\tbegin\n'
+        code += '\t\tsystem_failure <= 1\'b1;\n'
+        code += '\t\tstate <= ' + init_state + ';\n'
+        code += '\t\tend\n'
+        code += '\tendcase\n'
+        code += 'end\n'
         code += 'endmodule'
+        return code
 
 
+Sample_transition = {
+    1: {'e1': 2, 'e2': 3, 'e3': 2},
+    2: {'e1': 3, 'e2': 1, 'e3': 3},
+    3: {'e1': 1, 'e2': 2, 'e3': 1}
+}
+
+sys = System(states=[1, 2, 3], transition=Sample_transition)
+gen = Generator()
+code = gen.generate_verilog_code(sys=sys)
+print(code)
